1b46fb4cbae8b3ca5c138f664ed749316784af73
Luiz Fernando <99697056+Lui8silva@users.noreply.github.com>
2022-04-15 00:48:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask('app')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String())
+    created_at = db.Column(db.String())
+    update_at = db.Column(db.String())
+
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    phone = db.Column(db.String())
+    image = db.Column(db.String())
+    user_id = db.Column(db.Integer)
+    created_at = db.Column(db.String())
+    update_at = db.Column(db.String())
+
+@app.route('/')
+def index():
+    contacts = Contacts.query.all()
+    return render_template(
+      'index.html',
+    contacts = contacts
+                        )
+
+@app.route('/create', methods=['POST'])
+def create():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    new_contacts = Contacts(
+        name = name,
+        email = email,
+        phone = phone
+    )
+    db.session.add(new_contacts)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    delete = Contacts.query.filter_by(id=id).first()
+    db.session.delete(delete)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/update/<int:id>', methods=['POST'])
+def update(id):
+    name = request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    update = Contacts.query.filter_by(id=id).first()
+    update.name = name
+    update.email = email
+    update.phone = phone
+    db.session.commit()
+    return redirect('/')
+    
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(host='0.0.0.0', port=8080)
